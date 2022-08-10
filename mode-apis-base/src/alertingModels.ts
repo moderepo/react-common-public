7e005f87964a56d55def366027ff688ccbe693e0
bill-mode <53446239+bill-mode@users.noreply.github.com>
2022-08-10 02:54:14 -0700

    change targetMetrics to targetMetric

--- mode-apis-base/src/alertingModels.ts
+++ mode-apis-base/src/alertingModels.ts
@@ -101,8 +101,8 @@ export interface AlertTargetMetric {
 export interface MetricsAlertCondition extends AlertCondition {
     readonly conditionType: AlertConditionType.METRIC_HEARTBEAT | AlertConditionType.METRIC_THRESHOLD;
 
-    // Metrics that are monitored.
-    readonly targetMetrics: [AlertTargetMetric, ...AlertTargetMetric[]];
+    // The Metric that is being monitored.
+    readonly targetMetric: AlertTargetMetric;
 }
 
 
@@ -171,7 +171,7 @@ export interface AlertMessageTemplate {
 
 export enum AlertRecipientType {
     EMAIL = 'email'
-};
+}
 
 export interface AlertRecipient {
     readonly recipientType: AlertRecipientType;
